5fad245da5d2aed17bc8b04afcf402d6490f1f8e
NetrunnerFox <107522945+NetrunnerFox@users.noreply.github.com>
2024-01-08 21:44:23 -0300

    Versão 1.0

---
+++ core.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper-desk", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
